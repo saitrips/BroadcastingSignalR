fd968bc74496f7b77d7f8f1d747b71e09f311f41
saitrips <sai_e@outlook.com>
2020-11-27 15:00:23 -0500

    add signalr broadcasting

---
+++ Agent/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Agent
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HubConnection connection = new HubConnectionBuilder()
+                //.WithUrl("http://localhost:18128/ChatHub")
+                .WithUrl("http://localhost:8080/ChatHub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            await connection.StartAsync();
+
+            string message = Console.ReadLine();
+
+            await connection.InvokeAsync("SendMessage", "Agent", message);
+
+        }
+     
+    }
+}
